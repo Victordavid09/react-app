a5fb00998c7f5c530e64366ea3ffbc321586ab4a
Brandon Munongi <brandon.munongi@gmail.com>
2024-01-03 18:06:05 +0200

    Creating a Button exercise

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
 
   return (
     <div>
-      <Alert>
-        Hello <span>World</span>
-      </Alert>
+      <Button onClick={() => console.log('clicked')}>My Button</Button>
     </div>
   );
 }
