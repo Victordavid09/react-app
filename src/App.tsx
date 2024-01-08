0e09d9e0c096b5ed04e63cec7fac703a0720a064
Brandon Munongi <brandon.munongi@gmail.com>
2024-01-08 16:52:59 +0200

    Schema based validation with Zod.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import { useState } from "react";
-import Button from "./components/Button";
-import Alert from "./components/Alert";
+import Forms from "./components/Forms";
 
 function App() {
-  const [alertVisible, setAlertVisibility] = useState(false);
 
   return (
     <div>
-      { alertVisible && <Alert onClose= {() => setAlertVisibility(false)} >My Alert</Alert>}
-      <Button onClick={() => setAlertVisibility(true) }>My Button</Button>
+      <Forms />
     </div>
   );
 }
